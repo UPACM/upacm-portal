44d6c16a46f8ff0f4281de77e1d3073ecbd04abc
Aikiro42 <38589158+Aikiro42@users.noreply.github.com>
2021-02-03 16:12:38 +0800

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,14 @@
 import React from "react"
 import Header from "../components/header.js"
+import Footer from "../components/footer.js"
 import "../styles/var.css"
 import "../styles/global.css"
 
 export default function Home() {
 	return (
 		<>
-			<Header />
-			<h1>bitch</h1>
+			<Header gap={true}/>
+			<Footer />
 		</>
 	)
 }
