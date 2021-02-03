bbacf7eb0fc998a876fbea01a2f92de29db9880f
Aikiro42 <38589158+Aikiro42@users.noreply.github.com>
2021-02-04 01:40:33 +0800

    Added working mobile navbar dropdown
    Funnily enough it appears from the right, not from the top. It's literally not a dropdown, but the way it functions makes it one.
    
    The scroll function isn't working, though.

--- src/components/header.js
+++ src/components/header.js
@@ -7,41 +7,60 @@ import "../styles/components/header.css"
 import siteLogo from "../assets/images/logo.png"
 import hamburgerIcon from "../assets/images/hamburger-icon.png"
 
-export default function Header(){
-	var isVisible = false
-	var visibleClass = ""
-	function handleClick(e){
-		isVisible = !isVisible
-		if(isVisible){
-			visibleClass = "visible"
-		}else{
-			visibleClass = ""
-		}
-		console.log(visibleClass)
+class Header extends React.Component{
+	
+	constructor(props){
+		super(props);
+		this.state = {isScrolled: false, navbarVisible: false};
+		this.handleClick = this.handleClick.bind(this);
+		this.handleScroll = this.handleScroll.bind(this);
 	}
-	return (
-		<>
-			<div class="main-portal header">
-				<div class="wrapper">
-					<div class="main-portal home-link">
-						<Link to="/">
-							<img class="main-portal logo" src= {siteLogo} />
-							<div class="main-portal header-text">
-								<span>ASSOCIATION FOR COMPUTING MACHINERY</span>
-								<span class="bottom-text">University of the Philippines Diliman Student Chapter, Inc.</span>
-							</div>
-						</Link>
-					</div>
-					<div class="navbar-button-container" onClick={handleClick}>
-						<img class="navbar-button" src={hamburgerIcon} />
-					</div>
-					<div class={"main-portal nav-bar " + visibleClass} id="main-portal-nav-bar">
-						<Navlink to="/" name="About" />
-						<Navlink to="/committees/" name="Committees" />
-						<Navlink to="/" name="Contact" />
+	
+	handleScroll(){
+		this.setState(
+			state => ({
+				isScrolled: !state.isScrolled
+			})
+		);
+		console.log("Scrolled");
+	}
+	
+	handleClick(){
+		console.log("Clicked.")
+		this.setState(
+			state => ({
+				navbarVisible: !state.navbarVisible
+			})
+		);
+	}
+	
+	render(){
+		return (
+			<>
+				<div class={this.state.isScrolled ? 'main-portal header scrolled' : 'main-portal header'} onscroll={this.handleScroll}>
+					<div class="wrapper">
+						<div class="main-portal home-link">
+							<Link to="/">
+								<img class="main-portal logo" src= {siteLogo} />
+								<div class="main-portal header-text">
+									<span>ASSOCIATION FOR COMPUTING MACHINERY</span>
+									<span class="bottom-text">University of the Philippines Diliman Student Chapter, Inc.</span>
+								</div>
+							</Link>
+						</div>
+						<div class="navbar-button-container" onClick={this.handleClick}>
+							<img class="navbar-button" src={hamburgerIcon} />
+						</div>
+						<div class={this.state.navbarVisible ? 'main-portal nav-bar visible' : 'main-portal nav-bar'} id="main-portal-nav-bar">
+							<Navlink to="/" name="About" />
+							<Navlink to="/committees/" name="Committees" />
+							<Navlink to="/" name="Contact" />
+						</div>
 					</div>
 				</div>
-			</div>
-		</>
-	)
+			</>
+		)
+	}
 }
+
+export default Header
