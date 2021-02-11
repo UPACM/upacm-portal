8083f3309bbc8b2664cc0b86846aa9e04a5d4acf
Aikiro42 <38589158+Aikiro42@users.noreply.github.com>
2021-02-11 14:14:53 +0800

    Added history
    Warning: Not yet responsive.

---
+++ src/components/about/history.js
@@ -0,0 +1,27 @@
+import React from "react"
+
+import HistoryTimestamp from "./subcomponents/history-timestamp.js"
+import HistoryData from "../../json/about/history.json"
+
+import "../../styles/var.css"
+import "../../styles/global.css"
+import "./history.css"
+
+export default function History(){
+	return (
+		<div class="main-portal" id="history-section">
+			<div class="wrapper">
+				<div id="history-intro">
+					<h1>History</h1>
+					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+				</div>
+				<div id="timeline-container">
+					{HistoryData.history.map((data) => {
+						var d = new Date(data.date);
+						return <HistoryTimestamp date={d.getFullYear()} event={data.event} />
+					})}
+				</div>
+			</div>
+		</div>
+	)
+}
