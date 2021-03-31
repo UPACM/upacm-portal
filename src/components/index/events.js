4854e9e452097a97bc5bcaac335f5f244b179aa5
Aikiro42 <38589158+Aikiro42@users.noreply.github.com>
2021-03-31 23:06:02 +0800

    Changed events style
    Hover could use some work

--- src/components/index/events.js
+++ src/components/index/events.js
@@ -1,33 +1,49 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { Link } from "gatsby"
 
 import EventData from "../../json/events.json"
+import Opening from "./opening.js"
+
 import "./events.scss"
+
+import AOS from "aos"
+import "aos/dist/aos.css"
 
 function EventDiamond({ event }) {
   return (
     <div class="diamond">
       <div class="diamond-content">
-        <img src={event.imageSource} />
+        <img class="diamond-img" src={event.imageSource} />
       </div>
     </div>
   )
 }
 
 export default function Events({ eventsSectionText }) {
+  useEffect(() => {
+    AOS.init()
+    AOS.refresh()
+  });
+
   return (
+    /*
+    
+        data-aos="fade-up"
+        data-aos-delay="80"
+        data-aos-duration="650"
+    
+        */
     <div id="events">
-      <div id="info">
-        <h1>Events</h1>
-        <p>{eventsSectionText}</p>
-        <Link className="events-page-link">LEARN MORE</Link>
-      </div>
-      <div class="showcase-wrapper">
+      <Opening 
+        header="Events"
+        headerInfo={eventsSectionText}
+        //aosFade={true}
+      />
+      <Link className="events-page-link">LEARN MORE</Link>
       <div id="showcase">
         {Object.keys(EventData).map(type => (
           <EventDiamond event={EventData[type].featured} />
         ))}
-      </div>
       </div>
     </div>
   )
