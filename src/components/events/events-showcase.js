15263ce34aa7d15f0ee660fd35190dfa3e46183a
ayumu <stephen.singer.098@gmail.com>
2021-04-02 16:39:09 +0800

    Adjust events design

--- src/components/events/events-showcase.js
+++ src/components/events/events-showcase.js
@@ -1,14 +1,30 @@
-import React from "react"
+import React, { useEffect } from "react"
 import "./events-showcase.scss"
 
 import EventsFeaturedCard from "./subcomponents/events-featured-card.js"
 import EventsCard from "./subcomponents/events-card.js"
 
+// On scroll animations
+import AOS from "aos"
+import "aos/dist/aos.css"
+
 export default function EventsShowcase({ events }) {	
-	
+	useEffect(() => {
+		AOS.init()
+		AOS.refresh()
+	  })
+
 	return <div class="events-showcase">
-		<EventsFeaturedCard event = {events.featured} />
-		<div class="unfeatured">
+		<div class="featured"
+			data-aos="fade-up"
+			data-aos-delay="80"
+			data-aos-duration="550">
+			<EventsFeaturedCard event = {events.featured} />
+		</div>
+		<div class="unfeatured"
+			data-aos="fade-up"
+			data-aos-delay="90"
+			data-aos-duration="550">
 			{events.unfeatured.map(event =>
 				<EventsCard event={event} />)}
 		</div>
