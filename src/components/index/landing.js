fa198156176965c9078b4ed6ebe8e6d90fb0ebeb
Aikiro42 <38589158+Aikiro42@users.noreply.github.com>
2021-04-07 12:30:07 +0800

    Improved landing "performance"
    Added blurred version of video before video loads, hopefully this will improve lighthouse performance score

--- src/components/index/landing.js
+++ src/components/index/landing.js
@@ -1,11 +1,13 @@
 import React from "react"
 //import sourceVid from "../assets/videos/landing_video.mp4"
 import "./landing.scss"
+import landingFade from "../../assets/videos/landing-video-fade.webp"
 
 export default function Landing({ videoSrcObj, introText, introSubText }) {
   return (
     <>
       <div class="landing incompressible" id="landing-video">
+        <img src={landingFade} id="landing-video-fade" />
         <video preload="auto" loop="true" autoplay="true" muted="true">
           <source type="video/mp4" src={videoSrcObj} />
           Your browser does not support the window tag.
