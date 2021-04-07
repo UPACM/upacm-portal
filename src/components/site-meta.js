e8348b3064c5ced5b60b5da4e6783e04783e9a42
Aikiro42 <38589158+Aikiro42@users.noreply.github.com>
2021-04-07 11:52:44 +0800

    Update site-meta.js
    Changed canonical link to `https://upacmportalmaster.gatsbyjs.io` - Idk if this is the real canonical link but idk i think it is

--- src/components/site-meta.js
+++ src/components/site-meta.js
@@ -14,7 +14,7 @@ export default function SiteMeta({ pageName, sitePage, isIndex }) {
     <Helmet>
       <meta charSet="utf-8" />
       {titleTag}
-      <link rel="canonical" href={"https://upacm.net" + sitePage} />
+      <link rel="canonical" href={"https://upacmportalmaster.gatsbyjs.io" + sitePage} />
       <meta
         name="Description"
         content="We envision a world where computing stands at the forefront of development - where we are able to use our skills to address the problems of tomorrow and create a positive impact on the community at large."
