8982f0dead0cacfd0a63164bc84cd25b9bbdd3e1
Aikiro42 <38589158+Aikiro42@users.noreply.github.com>
2021-04-12 18:18:13 +0800

    Split twitter js
    Used `@loadable/component` to lazily load twitter js

--- src/components/index/social-embed.js
+++ src/components/index/social-embed.js
@@ -1,4 +1,7 @@
 import React from "react"
+import loadable from '@loadable/component'
+
+const TwitterEmbed = loadable(() => import('../twitter-embed.js'))
 
 import "./social-embed.scss"
 
@@ -7,18 +10,7 @@ export default function SocialEmbed() {
   return (
     <div id="social-embed">
       <div id="twitter">
-	  	<a 
-		  data-dnt="true" 
-		  data-height="550" 
-		  className="twitter-timeline" 
-		  href="https://twitter.com/upacm?ref_src=twsrc%5Etfw">
-	      Tweets by upacm
-		</a>
-		<script 
-		  async 
-		  charSet="utf-8"
-		  src="https://platform.twitter.com/widgets.js"
-		/> 
+	  	<TwitterEmbed />
       </div>
     </div>
   )
