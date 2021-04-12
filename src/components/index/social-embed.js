3a5d4b7168bcbb3a3c4ce821675c930b7fdbf9df
Aikiro42 <38589158+Aikiro42@users.noreply.github.com>
2021-04-12 18:26:36 +0800

    Reordered imports
    there was an import statement after the tiwitter embed lazy loading, moved it

--- src/components/index/social-embed.js
+++ src/components/index/social-embed.js
@@ -1,9 +1,10 @@
 import React from "react"
 import loadable from '@loadable/component'
-
-const TwitterEmbed = loadable(() => import('../twitter-embed.js'))
-
 import "./social-embed.scss"
+
+
+const TwitterEmbed = loadable(() => import('../twitter-embed'))
+
 
 /* Component for referencing latest media posts */
 export default function SocialEmbed() {
