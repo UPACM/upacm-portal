d8933acda878a3d3bf35b2496a7d9da8a85a5853
ayumu <stephen.singer.098@gmail.com>
2021-04-16 18:38:00 +0800

    Add on scroll animation to gallery

--- src/components/gallery/subcomponents/gallery-card.js
+++ src/components/gallery/subcomponents/gallery-card.js
@@ -1,13 +1,16 @@
 import React from "react"
+import Fade from 'react-reveal/Fade';
 
 import "./gallery-card.scss"
 
 /* Subcomponent for rendering a special interest group */
 export default function GalleryCard({ image }) {
   return (
-    <div className="gallery-card">
-      <img src={image.source} alt="" />
-      <p className="caption">{image.caption}</p>
-    </div>
+	<Fade ssrFadeout bottom duration={1000}>
+      <div className="gallery-card">
+        <img src={image.source} alt="" />
+        <p className="caption">{image.caption}</p>
+      </div>
+	</Fade>
   )
 }
