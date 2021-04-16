0e99e8feab3dd261f313c3d42ced045c739baf42
ayumu <stephen.singer.098@gmail.com>
2021-04-16 18:59:06 +0800

    Add on-scroll animation to event cards

--- src/components/events/subcomponents/events-featured-card.js
+++ src/components/events/subcomponents/events-featured-card.js
@@ -1,18 +1,21 @@
 import React from "react"
+import Reveal from "react-reveal/Reveal"
 
 import "./events-featured-card.scss"
 
 export default function EventsFeaturedCard({ event }) {
   return (
-    <div className="events-featured-card" id={event.header}>
-      <img src={event.imageSource} alt={event.header} />
-      <div id="info">
-        <h5 id="header">{event.header}</h5>
-        <h6 id="subheader">
-          <i>{event.subheader}</i>
-        </h6>
-        <p id="description">{event.info}</p>
+    <Reveal bottom duration={400} delay={150}>
+      <div className="events-featured-card" id={event.header}>
+        <img src={event.imageSource} alt={event.header} />
+        <div id="info">
+          <h5 id="header">{event.header}</h5>
+          <h6 id="subheader">
+            <i>{event.subheader}</i>
+          </h6>
+          <p id="description">{event.info}</p>
+        </div>
       </div>
-    </div>
+    </Reveal>
   )
 }
