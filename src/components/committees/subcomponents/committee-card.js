479058a2d7e62d2c7513287890de145cf47b6e7d
ayumu <stephen.singer.098@gmail.com>
2021-04-16 18:48:30 +0800

    Add on scroll animation to committee

--- src/components/committees/subcomponents/committee-card.js
+++ src/components/committees/subcomponents/committee-card.js
@@ -1,4 +1,5 @@
 import React from "react"
+import Reveal from 'react-reveal/Reveal';
 
 import "./committee-card.scss"
 
@@ -15,16 +16,18 @@ function BoldItalic({ name, text }) {
 /* Subcomponent for rendering one member of a committee type*/
 export default function CommitteeCard({ member }) {
   return (
-    <div className="committee-card">
-      <div className="portrait-wrapper">
-        <img alt="member" className="portrait" src={member.portraitSrc} />
+	<Reveal bottom duration={550} delay={400}>
+      <div className="committee-card">
+       <div className="portrait-wrapper">
+          <img alt="member" className="portrait" src={member.portraitSrc} />
+        </div>
+        <p className="name">
+          <b>{member.name}</b>
+        </p>
+        {member.position.map(position => (
+          <BoldItalic key={position} name="position" text={position} />
+        ))}
       </div>
-      <p className="name">
-        <b>{member.name}</b>
-      </p>
-      {member.position.map(position => (
-        <BoldItalic key={position} name="position" text={position} />
-      ))}
-    </div>
+	</Reveal>
   )
 }
