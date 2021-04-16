c1ae6d279f8358349a8b3eacf7492b8fe1ebfa56
ayumu <stephen.singer.098@gmail.com>
2021-04-16 19:26:24 +0800

    Add on-scroll animation to SIG cards

--- src/components/sig/subcomponents/sig-card.js
+++ src/components/sig/subcomponents/sig-card.js
@@ -1,32 +1,35 @@
 import React from "react"
+import Fade from "react-reveal/Fade"
 
 import "./sig-card.scss"
 
 /* Subcomponent for rendering a special interest group */
 export default function SigCard({ group }) {
   return (
-    <div
-      className="sig-card"
-      style={{ backgroundImage: "url(" + group.portraitSrc + ")" }}
-    >
-      <div className="info">
-        <h2 className="name">
-          <b>{group.name}</b>
-        </h2>
-        <h3 className="head">
-          <i>{group.head}</i>
-        </h3>
-        {group.description.map(paragraph => (
-          <p key={paragraph} className="description">
-            {paragraph}{" "}
-          </p>
-        ))}
-        <div className="ripples">
-          <div className="ripple" />
-          <div className="ripple" />
-          <div className="ripple" />
+    <Fade ssrFadeout bottom duration={650} delay={100}>
+      <div
+        className="sig-card"
+        style={{ backgroundImage: "url(" + group.portraitSrc + ")" }}
+      >
+        <div className="info">
+          <h2 className="name">
+            <b>{group.name}</b>
+          </h2>
+          <h3 className="head">
+            <i>{group.head}</i>
+          </h3>
+          {group.description.map(paragraph => (
+            <p key={paragraph} className="description">
+              {paragraph}{" "}
+            </p>
+          ))}
+          <div className="ripples">
+            <div className="ripple" />
+            <div className="ripple" />
+            <div className="ripple" />
+          </div>
         </div>
       </div>
-    </div>
+    </Fade>
   )
 }
