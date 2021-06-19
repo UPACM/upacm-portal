d143084157dadc9f50b1bf04b7a4af808d817860
ayumu <stephen.singer.098@gmail.com>
2021-06-19 15:11:25 +0800

    Update gallery to slideshow

--- src/components/gallery/gallery-cards.js
+++ src/components/gallery/gallery-cards.js
@@ -1,15 +1,32 @@
 import React from "react"
+import Slider from "react-slick";
 import GalleryCard from "./subcomponents/gallery-card"
+import Fade from "react-reveal/Fade"
 
 import "./gallery-cards.scss"
 
 /* Component for opening a page section */
 export default function GalleryCards({ images }) {
-  return (
-    <div id="gallery-cards">
-      {images.map(image => (
-        <GalleryCard key={image.caption} image={image} />
-      ))}
+  const settings = {
+		dots: true,
+		infinite: true,
+		speed: 500,
+		slidesToShow: 2,
+		slidesToScroll: 1,
+    lazyLoad: true,
+  };	
+  
+    return (
+      <div id="gallery-cards">
+      <Fade ssrFadeout bottom duration={650} delay={100}>
+        <Slider {...settings}>
+          {images.map(imageSource => (
+            <GalleryCard key={imageSource} source={imageSource} />
+          ))}
+        </Slider>
+      </Fade>
     </div>
-  )
+    )
+    
 }
+
